d116628079fcb35d9f5eb3db72a249f706d9e91d
Riad Shash <riadshash@yahoo.com>
2019-10-05 11:56:13 -0600

    added in progress hw5

---
+++ Lighting/project.c
@@ -0,0 +1,28 @@
+/*
+ *  Set projection
+ */
+#include "CSCIx229.h"
+
+void Project(double fov, double asp, double dim, int projectionMode)
+{
+  //  Tell OpenGL we want to manipulate the projection matrix
+  glMatrixMode(GL_PROJECTION);
+  //  Undo previous transformations
+  glLoadIdentity();
+
+  if(projectionMode == 1){
+    //  First person
+    gluPerspective(fov,asp,dim/6,10*dim);
+  }if (projectionMode == 2){
+    //  Perspective transformation
+    gluPerspective(fov,asp,dim/4,4*dim);
+  }else if (projectionMode == 3){
+    //  Orthogonal projection
+    glOrtho(-asp*dim,+asp*dim, -dim,+dim, -dim,+dim);
+  }
+  
+  //  Switch to manipulating the model matrix
+  glMatrixMode(GL_MODELVIEW);
+  //  Undo previous transformations
+  glLoadIdentity();
+}
